f5612be428a3cc4a40367baadb3f31c1488291bc
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-08-30 14:56:58 -0700

    working on index and header

---
+++ public/src/components/app/HomepageApp.js
@@ -0,0 +1,11 @@
+import Component from '../Component.js';
+
+class Homepage extends Component {
+    renderHTML() {
+        return /*html*/`
+            
+        `;
+    }
+}
+
+export default Homepage;
---
+++ public/src/index.js
@@ -0,0 +1,4 @@
+import HomepageApp from './components/app/HomepageApp.js';
+
+const app =  new HomepageApp();
+document.body.prepend(app.renderHTML());
