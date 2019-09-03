b7706e660ad79f8fc4d3e31a928fa8a576597136
ollie <leyechi@gmail.com>
2019-09-03 14:30:18 -0700

    rendering gameplay header

---
+++ public/data/pokemonData.js
@@ -0,0 +1,16 @@
+const pokemonData = [
+    {
+        pokemon: 'user',
+        attack: 44,
+        defense: 30,
+        hp: 50,        
+    },
+    {
+        pokemon: 'enemy',
+        attack: 44,
+        defense: 30,
+        hp: 50,        
+    },
+];
+
+export default pokemonData;
