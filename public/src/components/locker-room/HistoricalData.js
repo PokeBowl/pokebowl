da8030042b7c1fe028df5be2084ef119f967423c
Joe Klause <joeklause@gmail.com>
2019-09-03 16:48:43 -0700

    locker room Components Rendered

---
+++ public/src/components/locker-room/HistoricalData.js
@@ -0,0 +1,11 @@
+import Component from '../Component.js';
+
+class HistoricalData extends Component {
+    renderHTML() {
+        return /*html*/`
+        <span id="historical-data-span">Historical Data Goes Here</span>
+        `;
+    }
+}
+
+export default HistoricalData;
