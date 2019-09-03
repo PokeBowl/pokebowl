50151650f25ce3c746fc8b3602d4ef4deb138beb
ollie <leyechi@gmail.com>
2019-09-03 15:12:22 -0700

    rendering user and opponent pokemon

--- public/data/pokemonData.js
+++ public/data/pokemonData.js
@@ -3,13 +3,15 @@ const pokemonData = [
         pokemon: 'user',
         attack: 44,
         defense: 30,
-        hp: 50,        
+        hp: 50,
+        url_image: '../public/assets/poke-avatar/antonella-avatar.png'      
     },
     {
         pokemon: 'enemy',
         attack: 44,
         defense: 30,
-        hp: 50,        
+        hp: 50, 
+        url_image: '../public/assets/poke-avatar/joe-avatar.png'    
     },
 ];
 
