da361c5213c208ff1522ecf6eddb09162f4fc85b
Joe Klause <joeklause@gmail.com>
2019-09-03 17:54:34 -0700

    css for locker room

--- public/src/components/app/LockerRoomApp.js
+++ public/src/components/app/LockerRoomApp.js
@@ -7,33 +7,39 @@ import pokemonData from '../../../data/pokemonData.js';
 
 class LockerRoomApp extends Component {
     onRender(dom) {
-        const userPokemon = pokemonData[0];
+        const headerRoot = dom.querySelector('#header-root');
         const historicalDataContainer = dom.querySelector('#historical-data-container');
         const userPokemonContainer = dom.querySelector('#user-pokemon-container');
-
+        
         const header = new Header();
-        dom.prepend(header.renderDOM());
-
+        headerRoot.prepend(header.renderDOM());
+        
         const historicalData = new HistoricalData();
         historicalDataContainer.appendChild(historicalData.renderDOM());
-
+        
+        const userPokemon = pokemonData[0];
         const props = { pokemon: userPokemon };
+
         const lockerRoomPokemon = new UserPokemon(props);
         userPokemonContainer.appendChild(lockerRoomPokemon.renderDOM());
     }
-    
+
     renderHTML() {
         return /*html*/`
-            <main>
-                <div id="user-pokemon-container"></div>
-                <div id="historical-data-container"></div>
-                <div id="how-to-play-container">
-                    <p id="instructions">
-                    Game Play Instructions Go Here
-                    </p>
-                    <button>Enter the PokeBowl</button>
-                </div>
-            </main>
+        <div>
+            <div id="header-root">
+                <main>
+                    <div id="user-pokemon-container"></div>
+                    <div id="historical-data-container"></div>
+                    <div id="how-to-play-container">
+                        <p id="instructions">
+                        Game Play Instructions Go Here
+                        </p>
+                        <button>Enter the PokeBowl</button>
+                    </div>
+                </main>
+            </div>
+        </div>
         `;
     }
 }
