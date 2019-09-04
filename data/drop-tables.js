d1848e10f9cd01d9715f54909273a1058b15fd9c
Joe Klause <joeklause@gmail.com>
2019-09-04 13:43:59 -0700

    set up user-pokemon-stats table

--- data/drop-tables.js
+++ data/drop-tables.js
@@ -4,6 +4,7 @@ const client = require('../lib/client');
 
 client.query(`
     DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS user-pokemon-stats;
 `)
     .then(
         () => console.log('drop tables complete'),
