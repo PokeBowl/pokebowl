51b2670a8f938f978c4fc31e0df527987a0f74f0
ollie <leyechi@gmail.com>
2019-09-04 14:27:48 -0700

    pokemon local storage

--- public/src/services/store.js
+++ public/src/services/store.js
@@ -1,3 +1,8 @@
+import pokemonData from '../../data/pokemonData.js';
+
+const USER_POKEMON_KEY = 'user-pokemon';
+const OPPONENT_POKEMON_KEY = 'opponent-pokemon';
+
 const TOKEN_KEY = 'token';
 
 export default {
@@ -12,5 +17,29 @@ export default {
     },
     removeToken() {
         localStorage.removeItem(TOKEN_KEY);
+    },
+    getUserPokemonLS() {
+        return localStorage.getItem(USER_POKEMON_KEY);
+    },
+    setUserPokemonLS() {
+        return localStorage.setItem(USER_POKEMON_KEY, pokemonData[0]);
+    },
+    hasUserPokemonLS() {
+        return this.getUserPokemonLS() !== null;
+    },
+    removeUserPokemonLS() {
+        localStorage.removeItem(USER_POKEMON_KEY);
+    },
+    getOpponentPokemonLS() {
+        return localStorage.getItem(OPPONENT_POKEMON_KEY);
+    },
+    setOpponentPokemonLS() {
+        return localStorage.setItem(OPPONENT_POKEMON_KEY, pokemonData[1]);
+    },
+    hasOpponentPokemonLS() {
+        return this.getOpponentPokemonLS() !== null;
+    },
+    removeOpponentPokemonLS() {
+        localStorage.removeItem(OPPONENT_POKEMON_KEY);
     }
 };
