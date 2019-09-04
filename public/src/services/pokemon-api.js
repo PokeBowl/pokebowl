c5023763126e8cfcc7642b1d7becf63270b06370
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-09-04 12:01:45 -0700

    started to work on API call

--- public/src/services/pokemon-api.js
+++ public/src/services/pokemon-api.js
@@ -1,1 +1,14 @@
-// const URL = 'https://alchemy-pokedex.herokuapp.com/api/pokedex';
+const URL = 'https://alchemy-pokedex.herokuapp.com/api/pokedex';
+
+export function getUserPokemon() {
+    const url = `${URL}?page=1&perPage=25&attack=50&defense=50&hp=50&sort=hp&direction=asc`;
+
+    return fetch(url)
+        .then(response => response.json())
+        .then(console.log())
+
+        .catch(err => {
+            // eslint-disable-next-line no-console
+            console.log('fetch err:', err);
+        });
+}
