f1b7706c7086123f735d1471b0f704bb74ad8e2e
Phil Fitch <mostreluctantly@gmail.com>
2019-09-04 14:18:45 -0700

    partial history db

--- data/create-tables.js
+++ data/create-tables.js
@@ -9,6 +9,16 @@ client.query(`
         display_name VARCHAR(256) NOT NULL
     );
     
+    CREATE TABLE history (
+        id INTEGER NOT NULL,
+        user_char VARCHAR(256) NOT NULL,
+        opponent VARCHAR(256) NOT NULL,
+        result VARCHAR(256) NOT NULL,
+        CONSTRAINT history_user_id_fkey FOREIGN KEY (id)
+            references users (id) MATCH SIMPLE
+            ON UPDATE NO ACTION ON DELETE NO ACTION
+    )
+
 `)
     .then(
         () => console.log('create tables complete'),
