a9db53e7087eff3a761e211427972aa34d13bb30
Joe Klause <joeklause@gmail.com>
2019-09-06 09:37:21 -0700

    updated landing page link

--- public/src/components/app/HomepageApp.js
+++ public/src/components/app/HomepageApp.js
@@ -1,13 +1,19 @@
 import Component from '../Component.js';
 
 class HomepageApp extends Component {
+    onRender(dom) {
+        const enterButton = dom.querySelector('#enter-app');
+        enterButton.addEventListener('click', () => { 
+            window.location = `./auth.html`;
+        });
+    }
     renderHTML() {
         return /*html*/`
             <div class="home-main-container">
                 <div id="home-logo-container">
                     <img id="home-logo" src="./assets/pokebowl-header.png" alt="pokebowl-logo">
                 </div>
-                <button id="enter-app"><a href="./auth.html">Enter the PokéBowl</a></button>
+                <button id="enter-app">Enter the PokéBowl</button>
             </div>
         `;
     }
