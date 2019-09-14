8e28a16c331698cdde823f73a60aad2f84a4be53
martypdx <marty@martypdx.com>
2019-09-14 09:15:13 -0700

    grading and feedback

--- public/src/components/app/Header.js
+++ public/src/components/app/Header.js
@@ -8,12 +8,12 @@ class Header extends Component {
                     <div class="header-img-container">
                         <img class="header-image" src="./assets/pokebowl-header.png" alt="pokebowl-logo">
                     </div>
+
                     <div id="button-container">
                         <div class="log-out-container">
                             <button class="log-out-button"><a href="./auth.html">Log Out</a></button>
                         </div>
                         
-
                         <nav class="head-nav">
                             <a class="locker-room-nav lock-hide" href="./locker-room.html">Locker Room</a>
                             <a class="locker-room-nav" href="./about-us.html">About Us</a>
